d7da39e08de8a76ef41e8b4db7e567983125d064
aortmann <aortmann@despegar.com>
2022-01-07 13:49:57 -0300

    improvements

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { columbus5, AddressProviderFromJson, MARKET_DENOMS, Earn } from "@anchor-protocol/anchor.js";
 import { LCDClient, Dec } from "@terra-money/terra.js";
 import "dotenv/config";
+import moment from "moment";
+//@ts-ignore
 import fetch from 'node-fetch';
 
 interface EpochStateResponse {
@@ -10,8 +12,9 @@ interface EpochStateResponse {
 // https://lcd.terra.dev/swagger-ui/#/
 const myAddress = process.env.TERRA_ADDR;
 if (!myAddress) throw new Error(`Undefined .env value: 'TERRA_ADDR'`)
-const depositTx = process.env.TERRA_DEPOSIT_TX;
-if (!depositTx) throw new Error(`Undefined .env value: 'TERRA_DEPOSIT_TX'`)
+let depositTxs:string | string[] | undefined = process.env.TERRA_DEPOSIT_TX;
+if (!depositTxs) throw new Error(`Undefined .env value: 'TERRA_DEPOSIT_TX'`)
+depositTxs = depositTxs.split(',')
 const market = "uusd";
 const addressProvider = new AddressProviderFromJson(columbus5);
 const lcd = new LCDClient({ URL: 'https://lcd.terra.dev', chainID: 'columbus-5' });
@@ -30,12 +33,29 @@ console.log(`Checking address : ${process.env.TERRA_ADDR}`);
       },
     },
   );
-  let depositedBalance = await lcd.tx.txInfo(depositTx).then((res) => {
-    return res && res.logs && new Dec(res.logs[0].events.filter(e => e.type === 'wasm')[0].attributes.filter(a => a.key === 'deposit_amount')[0].value).div(1000000).toFixed(2) || null;
-  }, (err: any) => { console.log(err) });
+  let depositTime:any, depositSinceHours:any, depositSinceDays, depositSinceWeeks, depositSinceMonths;
+  let depositedBalance = new Dec(0);
+  let depositedBalanceRaw;
+
+  for (const tx of depositTxs) {
+    const { logs, timestamp } = await lcd.tx.txInfo(tx);
+    //const depositTime:any = new Date(new Date(res.timestamp).toLocaleString('es-AR', { timeZone: 'America/Argentina/Buenos_Aires' }));
+    //const dateNow:any = new Date(new Date().toLocaleString('es-AR', { timeZone: 'America/Argentina/Buenos_Aires' }));
+    
+    
+    depositTime = depositTime? `${depositTime} + ${moment(timestamp)}`: moment(timestamp);
+    const dateNow = moment();
+    depositSinceHours = depositSinceHours? `${depositSinceHours} + ${dateNow.diff(moment(timestamp), 'hours', true).toFixed(2)}` : dateNow.diff(moment(timestamp), 'hours', true).toFixed(2);
+
+    if(logs) {
+      depositedBalance = depositedBalance.add(new Dec(logs[0].events.filter(e => e.type === 'wasm')[0].attributes.filter(a => a.key === 'deposit_amount')[0].value).div(1000000));
+      depositedBalanceRaw = `${depositedBalanceRaw? `${depositedBalanceRaw} + ` : ''}${new Dec(logs[0].events.filter(e => e.type === 'wasm')[0].attributes.filter(a => a.key === 'deposit_amount')[0].value).div(1000000).toFixed(2)}`;
+    }
+  }
+
   let depositedBalanceMessage;
-  if(depositedBalance) {
-    depositedBalanceMessage = `Initial balance: ${depositedBalance} UST`;
+  if(depositTime && depositedBalance) {
+    depositedBalanceMessage = `Deposited: ${depositTime}\nDeposited since: ${depositSinceHours} hours ago\nInitial balance: ${depositedBalanceRaw} UST`;
     console.log(depositedBalanceMessage);
   }
 
@@ -53,9 +73,23 @@ console.log(`Checking address : ${process.env.TERRA_ADDR}`);
     totalYieldMessage = `Total yield: ${(new Dec(deposit).sub(depositedBalance)).toFixed(2)} UST`;
   }
 
+  const apy = await new Earn(lcd, new AddressProviderFromJson(columbus5)).getAPY({market: MARKET_DENOMS.UUSD});
+
+  if(depositTime && depositedBalance && depositSinceHours) {
+    const averagePerHour = `\n----------\n\nAverage per hour: ${(new Dec(deposit).mul(apy).div(8760)).toFixed(2)} UST`;
+    //@ts-ignore
+    const averagePerDay = `Average per day: ${(new Dec(deposit).mul(apy).div(365.25)).toFixed(2)} UST`;
+    //@ts-ignore
+    const averagePerWeek = `Average per week: ${(new Dec(deposit).mul(apy).div(52.1775)).toFixed(2)} UST`;
+    //@ts-ignore
+    const averagePerMonth = `Average per month: ${(new Dec(deposit).mul(apy).div(12)).toFixed(2)} UST`;
+    //@ts-ignore
+    const averagePerYear = `Average per year: ${(new Dec(deposit).mul(apy)).toFixed(2)} UST`;
+    totalYieldMessage += `\n${averagePerHour}\n${averagePerDay}\n${averagePerWeek}\n${averagePerMonth}\n${averagePerYear}`;
+  }
+
   console.log(totalYieldMessage)
 
-  const apy = await new Earn(lcd, new AddressProviderFromJson(columbus5)).getAPY({market: MARKET_DENOMS.UUSD});
   const APYMessage = `Current APY: ${(apy * 100).toFixed(2)}%`;
   console.info(APYMessage);
 
